a109134297827e6a74e9e7dee5d4439503c107ca
huynvn <Huy.Nguyen@navigosgroup.com>
2022-11-13 17:17:11 +0700

    Redux-Saga-06:Phan biet takeEvery and takeLastest

--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,10 +1,17 @@
 import { PayloadAction } from "@reduxjs/toolkit";
-import { takeEvery } from "redux-saga/effects";
-import { increment } from "./counterSlice";
+import { delay, put, takeEvery, takeLatest } from "redux-saga/effects";
+import {  incrementSaga, incrementSagaSuccess } from "./counterSlice";
 
-export function* log(action: PayloadAction) {
-  console.log('Log', action);
-}
+// export function* log(action: PayloadAction) {
+//   console.log('Log', action);
+// }
+
+function* handleIncrementSaga(action: PayloadAction<number>) {
+  // wait 2s
+  yield delay(2000);
+  console.log('Waiting done, dispatch action');
+  yield put(incrementSagaSuccess(action.payload));
+} 
 
 export default function* counterSaga() {
   console.log('counterSaga');
@@ -14,6 +21,8 @@ export default function* counterSaga() {
 
   // Muốn lắng nghe 1 action dc dispatch
   // yield takeEvery('counter/increment', log);
-  yield takeEvery(increment().type, log);
+
+  // yield takeLatest(incrementSaga.toString(), handleIncrementSaga);
+  yield takeEvery(incrementSaga.toString(), handleIncrementSaga);
   
 }
