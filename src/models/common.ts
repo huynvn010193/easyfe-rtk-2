77f2109d783c72cfbade734b92054a80773d03e1
huynvn <Huy.Nguyen@navigosgroup.com>
2022-11-26 16:44:34 +0700

    Redux Saga: 13 - Setup APIs for student resource in react typescript

--- src/models/common.ts
+++ src/models/common.ts
@@ -10,4 +10,11 @@ export interface ListResponse<T> {
   pagination: PaginationParams;
 }
 
-
+// Khai báo kiểu dữ liệu params
+export interface ListParams {
+  _page: number;
+  _limit: number;
+  _sort: string;
+  _order: 'asc' | 'desc';
+  [key: string]: any
+}
