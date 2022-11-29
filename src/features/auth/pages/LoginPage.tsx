42aa10df9a7199bfebf59e2b8c42219fa42498af
huynvn <Huy.Nguyen@navigosgroup.com>
2022-11-29 21:10:45 +0700

    Redux Saga 15: Add material UI LIB

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,7 +1,35 @@
 import * as React from 'react';
+import { Paper, makeStyles, Typography, Button } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
 
-export interface LoginPageProps {}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+  },
+  box: {
+    padding: theme.spacing(2),
+  },
+}));
 
-export default function LoginPage(props: LoginPageProps) {
-  return <div>LoginPage</div>;
+export default function LoginPage() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Paper elevation={1} className={classes.box}>
+        <Typography variant="h5" component="h1">
+          Student Managemnet
+        </Typography>
+        <Box>
+          <Button fullWidth variant="contained" color="primary">
+            Fake Login
+          </Button>
+        </Box>
+      </Paper>
+    </div>
+  );
 }
