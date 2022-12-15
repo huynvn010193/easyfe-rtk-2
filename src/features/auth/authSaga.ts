73239e8c3837cf6bb45c2ca324350324b130632c
huynvn010193 <hpn010193@gmail.com>
2022-12-16 00:44:41 +0700

    Pending code clip 17

--- src/features/auth/authSaga.ts
+++ src/features/auth/authSaga.ts
@@ -1,21 +1,31 @@
-import { PayloadAction } from "@reduxjs/toolkit";
-import { fork, take } from "redux-saga/effects";
-import { LoginPayload, authActions } from "./authSlice";
+import { PayloadAction } from '@reduxjs/toolkit';
+import { fork, take, delay } from 'redux-saga/effects';
+import { LoginPayload, authActions } from './authSlice';
 
 function* handleLogin(payload: LoginPayload) {
   console.log('handleLogin', payload);
+  yield delay(2000);
+  localStorage.setItem('access_token', 'fake_token');
 }
 
 function* handleLogout() {
   console.log('handleLogout');
+
+  // Xóa access_token
+  localStorage.removeItem('access_token');
 }
 
 function* watchLoginFlow() {
-  // Phải để vào lòng lặp vô tận -> luôn luôn lắng nghe 2 hành động Login và Logout
-  while(true) {
-    // TODO: Đợi User dispatch 1 action Login lên -> thì thực hiện fork 1 task handleLogin
-    const action: PayloadAction<LoginPayload> = yield take(authActions.login.type);
-    yield fork(handleLogin, action.payload);
+  // Phải để vào lòng lặp vô tận -> luôn luôn lắng nghe 2 hành động Login và Logout.
+  while (true) {
+    const isLoggedIn = Boolean(localStorage.getItem('access_token'));
+
+    // Nếu chưa login thì lắng nghe LOGIN.
+    if (!isLoggedIn) {
+      // TODO: Đợi User dispatch 1 action Login lên -> thì thực hiện fork 1 task handleLogin
+      const action: PayloadAction<LoginPayload> = yield take(authActions.login.type);
+      yield fork(handleLogin, action.payload);
+    }
 
     // TODO: Sau khi user Login xong thì watch đợi Action logout
     yield take(authActions.logout.type);
