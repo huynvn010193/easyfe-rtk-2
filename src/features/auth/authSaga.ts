c5e1e3d5604f676c289d0f7d155d52c58b6860f6
huynvn <Huy.Nguyen@navigosgroup.com>
2022-12-19 21:51:45 +0700

    Redux-saga 17: Cai dat 2 task login and logout

--- src/features/auth/authSaga.ts
+++ src/features/auth/authSaga.ts
@@ -1,15 +1,26 @@
 import { PayloadAction } from '@reduxjs/toolkit';
-import { fork, take, delay } from 'redux-saga/effects';
+import { fork, take, delay, call, put } from 'redux-saga/effects';
 import { LoginPayload, authActions } from './authSlice';
 
 function* handleLogin(payload: LoginPayload) {
-  console.log('handleLogin', payload);
-  yield delay(2000);
-  localStorage.setItem('access_token', 'fake_token');
+  try {
+    yield delay(500);
+    localStorage.setItem('access_token', 'fake_token');
+    
+    // Sau khi login xong thì dispatch action login Success lên store.
+    yield put(authActions.loginSuccess({
+      id: 1,
+      name: 'Easy FrontEnd'
+    }));
+  // redirect Admin page
+
+  } catch (error) {
+    yield put(authActions.loginFailed("error"));
+  }
 }
 
 function* handleLogout() {
-  console.log('handleLogout');
+  yield delay(500);
 
   // Xóa access_token
   localStorage.removeItem('access_token');
@@ -19,7 +30,7 @@ function* watchLoginFlow() {
   // Phải để vào lòng lặp vô tận -> luôn luôn lắng nghe 2 hành động Login và Logout.
   while (true) {
     const isLoggedIn = Boolean(localStorage.getItem('access_token'));
-
+    
     // Nếu chưa login thì lắng nghe LOGIN.
     if (!isLoggedIn) {
       // TODO: Đợi User dispatch 1 action Login lên -> thì thực hiện fork 1 task handleLogin
@@ -27,9 +38,11 @@ function* watchLoginFlow() {
       yield fork(handleLogin, action.payload);
     }
 
-    // TODO: Sau khi user Login xong thì watch đợi Action logout
+    // TODO: Sau khi user Login xong thì watch đợi Action logout -> dùng take.
     yield take(authActions.logout.type);
-    yield fork(handleLogout);
+
+    // Phải remove logout trước thì mới lắng nghe -> dùng call (cần xem lại).
+    yield call(handleLogout);
   }
 }
 
--- src/models/city.ts
+++ src/models/city.ts
@@ -1,4 +1,4 @@
 export interface City {
   code: string;
   name: string;      
-}
+} 
