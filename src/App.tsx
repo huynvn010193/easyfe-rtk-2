5d3909386c5b27cc2dfe53525648fa427bbf5867
huynvn <Huy.Nguyen@navigosgroup.com>
2022-12-25 00:41:22 +0700

    Redux-saga: 21

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import { Button } from '@material-ui/core';
 import { useAppDispatch } from 'app/hooks';
 import { NotFound, PrivateRoute } from 'components/Common';
 import { AdminLayout } from 'components/Layout';
-import { authActions } from 'features/auth/authSlice';
 import LoginPage from 'features/auth/pages/LoginPage';
-import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  const dispatch = useAppDispatch();
   return (
     <div>
-      <Button variant="contained" color="primary" onClick={() => dispatch(authActions.logout())}>
-        Logout
-      </Button>
       <Switch>
         <Route path="/login">
           <LoginPage />
--- src/components/Layout/Admin.tsx
+++ src/components/Layout/Admin.tsx
@@ -1,7 +1,44 @@
 import * as React from 'react';
+import { Box, makeStyles } from '@material-ui/core';
+import { Header, Sidebar } from 'components/Common';
 
 export interface AdminLayoutProps {}
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'grid',
+    gridTemplateRows: 'auto 1fr', // Header sẽ có giá trị auto, còn lại chiếm hết
+    gridTemplateColumns: '240px 1fr', // Sidebar cố định 300 , còn lại thì chiếm hết
+    gridTemplateAreas: `"header header" "sidebar main"`,
+    minHeight: '100vh',
+  },
+  header: {
+    gridArea: 'header',
+  },
+  sidebar: {
+    gridArea: 'sidebar',
+    borderRight: `1px solid ${theme.palette.divider}`,
+    backgroundColor: theme.palette.background.paper,
+  },
+  main: {
+    gridArea: 'main',
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(2, 3),
+  },
+}));
+
 export function AdminLayout(props: AdminLayoutProps) {
-  return <div>AdminLayout</div>;
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.header}>
+        <Header />
+      </Box>
+      <Box className={classes.sidebar}>
+        <Sidebar />
+      </Box>
+      <Box className={classes.main}>Main</Box>
+    </Box>
+  );
 }
