1a207a1512a273ca0e49f57bc965b7b9aab39eca
huynvn <Huy.Nguyen@navigosgroup.com>
2023-01-02 11:07:06 +0700

    Redux Saga: 28

--- src/features/student/index.tsx
+++ src/features/student/index.tsx
@@ -1,5 +1,41 @@
 import * as React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+import {
+  palette,
+  PaletteProps,
+  spacing,
+  SpacingProps,
+  typography,
+  TypographyProps,
+} from '@material-ui/system';
+import styled from 'styled-components';
+import { Switch, Route } from 'react-router-dom';
+import { ListPage } from './pages/ListPage';
+import { AddEditPage } from './pages/AddEditPage';
+
+const Box = styled.div<PaletteProps & SpacingProps & TypographyProps>`
+  ${palette}${spacing}${typography}
+`;
 
 export default function StudentFeature() {
-  return <div>Student Feature</div>;
+  // Dùng match tận dụng lại nexted router . để không cần gõ lại đường dẫn cha: admin/students
+  const match = useRouteMatch();
+
+  return (
+    <Box>
+      <Switch>
+        <Route path={match.path} exact>
+          <ListPage />
+        </Route>
+
+        <Route path={`${match.path}/add`}>
+          <AddEditPage />
+        </Route>
+
+        <Route path={`${match.path}/:studentId`}>
+          <AddEditPage />
+        </Route>
+      </Switch>
+    </Box>
+  );
 }
---
+++ src/features/student/pages/AddEditPage.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export interface AddEditPageProps {}
+
+export function AddEditPage() {
+  return <div>AddEditStudentPage</div>;
+}
