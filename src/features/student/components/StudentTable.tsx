b9b119050958ce8f37207f325090665dd9d45309
huynvn <Huy.Nguyen@navigosgroup.com>
2023-01-03 00:25:23 +0700

    Redux-saga: 30 - UI Student List

---
+++ src/features/student/components/StudentTable.tsx
@@ -0,0 +1,67 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { Button, Paper } from '@material-ui/core';
+import { Student } from 'models';
+
+export interface StudentTableListProps {
+  studentList: Student[];
+  onEdit?: (student: Student) => void;
+  onRemove?: (student: Student) => void;
+}
+
+const useStyles = makeStyles((theme) => ({
+  table: {},
+  edit: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+export function StudentTableList({ studentList, onEdit, onRemove }: StudentTableListProps) {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} size="small" aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Gender</TableCell>
+            <TableCell>Mark</TableCell>
+            <TableCell>City</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {studentList.map((student, idx) => (
+            <TableRow key={student.id}>
+              <TableCell>{student.id}</TableCell>
+              <TableCell>{student.name}</TableCell>
+              <TableCell>{student.gender}</TableCell>
+              <TableCell>{student.mark}</TableCell>
+              <TableCell>{student.city}</TableCell>
+              <TableCell align="right">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => onEdit?.(student)}
+                  className={classes.edit}
+                >
+                  Edit
+                </Button>
+                <Button variant="outlined" color="secondary" onClick={() => onRemove?.(student)}>
+                  Remove
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/features/student/pages/ListPage.tsx
+++ src/features/student/pages/ListPage.tsx
@@ -1,11 +1,38 @@
-import { useAppDispatch } from 'app/hooks';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
 import React, { useEffect } from 'react';
-import { studentActions } from '../studentSlice';
-
+import { selectStudentList, studentActions } from '../studentSlice';
+import {
+  palette,
+  PaletteProps,
+  spacing,
+  SpacingProps,
+  typography,
+  TypographyProps,
+} from '@material-ui/system';
+import styled from 'styled-components';
+import { Typography, Button, makeStyles } from '@material-ui/core';
+import { StudentTableList } from '../components/StudentTable';
 export interface AddEditPageProps {}
+
+const Box = styled.div<PaletteProps & SpacingProps & TypographyProps>`
+  ${palette}${spacing}${typography}
+`;
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  titleContainer: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: theme.spacing(4),
+  },
+}));
 
 export function ListPage() {
   const dispatch = useAppDispatch();
+  const classes = useStyles();
+  const studentList = useAppSelector(selectStudentList);
 
   useEffect(() => {
     dispatch(
@@ -16,5 +43,16 @@ export function ListPage() {
     );
   }, [dispatch]);
 
-  return <div>Student List Page</div>;
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.titleContainer}>
+        <Typography variant="h4">Students</Typography>
+        <Button variant="contained" color="primary">
+          Add new Student
+        </Button>
+      </Box>
+      {/* StudentTableList  */}
+      <StudentTableList studentList={studentList} />
+    </Box>
+  );
 }
