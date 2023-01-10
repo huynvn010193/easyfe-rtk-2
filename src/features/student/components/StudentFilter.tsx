728a63075f1f52baf3fd91334dc77872c03950eb
huynvn010193 <hpn010193@gmail.com>
2023-01-11 00:34:31 +0700

    Redux-saga: 36: Chức năng sort student

--- src/features/student/components/StudentFilter.tsx
+++ src/features/student/components/StudentFilter.tsx
@@ -6,10 +6,11 @@ import {
   OutlinedInput,
   Select,
   MenuItem,
+  Button,
 } from '@material-ui/core';
 import { Search } from '@material-ui/icons';
 import { City, ListParams } from 'models';
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useRef } from 'react';
 
 export interface StudentFilterProps {
   filter: ListParams;
@@ -20,6 +21,8 @@ export interface StudentFilterProps {
 }
 
 export function StudentFilter({ filter, cityList, onChange, onSearchChange }: StudentFilterProps) {
+  const searchRef = useRef<HTMLInputElement>();
+
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     // Do onSearch là optional => nên phải kiểm tra tồn tại
     if (!onSearchChange) return;
@@ -36,7 +39,6 @@ export function StudentFilter({ filter, cityList, onChange, onSearchChange }: St
 
   const handleCityChange = (e: ChangeEvent<{ name?: string; value: unknown }>) => {
     if (!onChange) return;
-
     const newFilter: ListParams = {
       ...filter,
       _page: 1,
@@ -44,6 +46,41 @@ export function StudentFilter({ filter, cityList, onChange, onSearchChange }: St
     };
 
     onChange(newFilter);
+  };
+
+  const handleSortChange = (e: ChangeEvent<{ name?: string; value: unknown }>) => {
+    if (!onChange) return;
+
+    const value = e.target.value;
+
+    const [_sort, _order] = (value as string).split('.');
+
+    // Ép kiểu order.
+    const newFilter: ListParams = {
+      ...filter,
+      _sort: _sort || undefined,
+      _order: (_order as 'asc' | 'desc') || undefined,
+    };
+
+    onChange(newFilter);
+  };
+
+  const handleClearFilter = () => {
+    if (!onChange) return;
+
+    const newFilter: ListParams = {
+      ...filter,
+      _page: 1,
+      city: undefined,
+      _sort: undefined,
+      _order: undefined,
+      name_like: undefined,
+    };
+
+    onChange(newFilter);
+    if (searchRef.current) {
+      searchRef.current.value = '';
+    }
   };
 
   return (
@@ -57,9 +94,13 @@ export function StudentFilter({ filter, cityList, onChange, onSearchChange }: St
               label="Search by name"
               endAdornment={<Search />}
               onChange={handleSearchChange}
+              defaultValue={filter.name_like}
+              inputRef={searchRef}
             />
           </FormControl>
         </Grid>
+
+        {/* FilterByCity  */}
         <Grid item xs={12} md={6} lg={3}>
           <FormControl variant="outlined" size="small" fullWidth>
             <InputLabel id="filterByCity">Filter by city</InputLabel>
@@ -80,6 +121,34 @@ export function StudentFilter({ filter, cityList, onChange, onSearchChange }: St
               ))}
             </Select>
           </FormControl>
+        </Grid>
+
+        {/* Sort */}
+        <Grid item xs={12} md={6} lg={2}>
+          <FormControl variant="outlined" size="small" fullWidth>
+            <InputLabel id="sortBy">Sort</InputLabel>
+            <Select
+              labelId="sortBy"
+              value={filter._sort ? `${filter._sort}.${filter._order}` : ''}
+              onChange={handleSortChange}
+              label="Sort"
+              fullWidth
+            >
+              <MenuItem value="">
+                <em>No sort</em>
+              </MenuItem>
+
+              <MenuItem value="name.asc">Name ASC</MenuItem>
+              <MenuItem value="name.desc">Name DESC</MenuItem>
+              <MenuItem value="mark.asc">Mark ASC</MenuItem>
+              <MenuItem value="mark.desc">Mark DESC</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} md={6} lg={1}>
+          <Button variant="outlined" color="primary" fullWidth onClick={handleClearFilter}>
+            Clear
+          </Button>
         </Grid>
       </Grid>
     </Box>
